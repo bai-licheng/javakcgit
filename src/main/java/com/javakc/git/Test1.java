9c068820a497fae1960b3afeaae5814786a49858
javakc_global <javakc_global@javakc.com>
2021-04-06 18:20:22 +0800

    mahuateng提交2

--- src/main/java/com/javakc/git/Test1.java
+++ src/main/java/com/javakc/git/Test1.java
@@ -3,6 +3,7 @@ package com.javakc.git;
 public class Test1 {
 
     private String mayun2;
+    private String mayun2_mahuateng;
     public static void main(String[] args) {
         System.out.println("123456");
     }
