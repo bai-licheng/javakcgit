2cb613b1de7bc03018761af57eb43b6103391b80
javakc_global <javakc_global@javakc.com>
2021-04-06 15:14:48 +0800

    这是第二次提交

--- src/main/java/com/javakc/git/Test1.java
+++ src/main/java/com/javakc/git/Test1.java
@@ -2,7 +2,8 @@ package com.javakc.git;
 
 public class Test1 {
 
+    private String mayun;
     public static void main(String[] args) {
-        System.out.println("123");
+        System.out.println("123456");
     }
 }
